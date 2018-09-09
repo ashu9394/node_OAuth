46328aedf83495e2a0d39837c77698a2d448147a
ashu9394 <ashu90394@gmail.com>
2018-09-09 16:28:46 +0530

    Added secret keys and google keys

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,7 +1,10 @@
 const passport = require('passport')
+const keys = require('./keys')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 
-// passport.use(new GoogleStrategy({
-//     clientID : 
-// }))
+passport.use(new GoogleStrategy({
+    clientID : keys.google.clientID,
+    clientSecret : keys.google.clientSecret
+
+}))
